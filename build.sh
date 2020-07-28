782d503c572535b7a1ccd0530ec764c7c762b8ac
IsraelWebDev <spl13@columbia.edu>
2020-07-28 20:08:56 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,9 +2,10 @@
 aws_region=$1
 package_bucket=$2
 stack_name=$3
-convert_pdf_bucket=$4
+input_bucket=$4
+output_bucket=$5
 
 aws --region $aws_region cloudformation package --template-file template.yaml --s3-bucket $package_bucket --output-template-file package.yaml
-aws --region $aws_region cloudformation deploy --template-file package.yaml --stack-name $stack_name --capabilities CAPABILITY_IAM --parameter-overrides BucketName=$convert_pdf_bucket
+aws --region $aws_region cloudformation deploy --template-file package.yaml --stack-name $stack_name --capabilities CAPABILITY_IAM --parameter-overrides InputBucketName=$input_bucket OutputBucketName=$output_bucket
 
 
